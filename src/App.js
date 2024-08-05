26588d9f3e260dcd922bc73068467f34ad8cdbea
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-08-04 21:51:04 -0300

    Atualizaçao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import  TabelaHead   from "./components/tabelaHead";
+import  TabelaFoot  from "./components/tabelaFoot";
+import  TabelaBody from "./components/tabelaBody";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends Component{
+
+state ={
+  livros: [
+  {
+
+    id: "12312-123123-412312",
+    titulo: "CSS Grid Layout",
+    autor: "Mauricio Samy Silva"
+    
+  },
+  {
+
+    id: "129483-934-393930",
+    titulo: "Node Essencial",
+    autor: "Ricardo R. Lecheta"
+  },
+  {
+  id: "94839-82349-3738",
+  titulo: "Aprendendo Material Design",
+  autor: "Kile Mew"
+  }    
+  
+]
+};
+render() {
+    return (
+<table className="tabela">
+<TabelaHead/>
+<TabelaBody livros={ this.state.livros }/>
+<TabelaFoot/>
+</table>
+   
   );
+}
 }
 
 export default App;
---
+++ src/components/tabelaFoot.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TabelaFoot = () => (
+<tfood>
+<tr>
+<td colSpan="4">Este é o rodapé da tabela</td>    
+</tr>
+</tfood>
+
+);
+
+
+export default TabelaFoot;
